df90cd5636c1c65c3bbbfd0401357a1466687e58
Maxim McNair <mail@maximmcnair.com>
2014-08-04 16:37:27 +0100

    REomve old files

--- js/controllers/project.js
+++
@@ -1,4 +0,0 @@
-angular.module('maximmcnairApp')
-  .controller('ProjectCtrl', function ($scope, $routeParams, ProjectsFactory) {
-    $scope.project = ProjectsFactory.getTask($routeParams.projectSlug)
-  })
--- js/controllers/projects.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('maximmcnairApp')
-  .controller('ProjectsCtrl', function ($scope, ProjectsFactory) {
-    $scope.projects = ProjectsFactory.get()
-    console.log($scope.projects)
-  })
