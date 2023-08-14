1fa128347e449a65eebe27c0f2a425c86c077c61
Maxim McNair <mail@maximmcnair.com>
2023-08-14 13:01:04 +0100

    duotone article updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.css"
+          integrity="sha384-WsHMgfkABRyG494OmuiNmkAOk8nhO1qE+Y6wns6v+EoNoTNxrWxYpl5ZYWFOLPCM"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
--- src/pages/p/[slug].tsx
+++ src/pages/p/[slug].tsx
@@ -6,7 +6,6 @@ import remarkMath from 'remark-math';
 import format from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
 import Highlight from 'react-highlight';
-import Head from 'next/head';
 import Link from 'next/link';
 
 import { getPostSlugs, getPost, getPosts } from '@/utils/posts';
@@ -83,15 +82,6 @@ const Post: NextPage<Props> = ({ meta, mdx, posts }) => {
   return (
     <Layout title={meta?.title} desc={meta?.desc}>
       <article className="article content">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.css"
-            integrity="sha384-WsHMgfkABRyG494OmuiNmkAOk8nhO1qE+Y6wns6v+EoNoTNxrWxYpl5ZYWFOLPCM"
-            crossOrigin="anonymous"
-          />
-        </Head>
-
         <header className="article__header">
           <h1 className="article__title">{meta?.title}</h1>
           <strong className="article__date">
@@ -115,6 +105,7 @@ const Post: NextPage<Props> = ({ meta, mdx, posts }) => {
               pre: Highlight,
               small: ({ children }) => <small>{children}</small>,
               ArticlePreview: ({ slug }) => {
+                return null;
                 const post = posts.find(p => p.slug === slug);
                 if (!post) return null;
                 return (
@@ -126,6 +117,7 @@ const Post: NextPage<Props> = ({ meta, mdx, posts }) => {
                       gap: '15px',
                     }}
                   >
+                    {/* @ts-ignore */}
                     <Article article={post} style={{ gridColumnStart: 2 }} />
                   </div>
                 );
